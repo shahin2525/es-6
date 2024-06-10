feb0f454acb3ef9484526c52f1b504b40129e55f
shahin2525 <shahin451999@gmail.com>
2024-06-09 21:10:37 -0700

    pracatice spread operatior

---
+++ default.js
@@ -0,0 +1,16 @@
+// const defaultFunction1 = (num = 0, num2 = 0) => {
+//   const result = num + num2;
+//   console.log(num, num2, result);
+//   return result;
+// };
+// console.log(defaultFunction1(5));
+
+// const defaultFunction1 = (numbs=[])=>{
+
+// }
+// const defaultFunction2 = (numbs={})=>{
+
+// }
+// const arrayFunction = (num1, num2) => num1 + num2;
+// const result = arrayFunction(10, 20);
+// console.log(result);
---
+++ spread.js
@@ -0,0 +1,7 @@
+const array1 = [10, 1, 11, 21, 32];
+const array2 = [...array1, 55];
+// console.log(array1, array2);
+const max = Math.max(6, 7, 8, 10, 2);
+const numbers = [4, 5, 45, 8];
+const max25 = Math.max(...[4, 5, 45, 8]);
+console.log(max, max25);
